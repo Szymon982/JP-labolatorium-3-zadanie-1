3a7043a26e621d9b90299f685d6ee4d8f8cd77bf
Szymon Borzdyński <106021186+Szymon982@users.noreply.github.com>
2022-10-13 09:55:36 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,35 +1,34 @@
 package com.company;
 
 import java.io.*;
-import java.lang.annotation.Target;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     static String st;
-    //static ArrayList<String> produkty = new ArrayList<String>();
-    static ArrayList<String> koszyk = new ArrayList<String>();
+    static ArrayList<String> koszyk = new ArrayList<>();
     static String[] Dane;
-    static ArrayList<String[]> Pelne_dane = new ArrayList<String[]>();
-    static ArrayList<produkt> Lista_produktow = new ArrayList<produkt>();
+    static ArrayList<produkt> Lista_produktow = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         System.out.println("Autor: Szymon Borzdyński");
-        File plik = new File("C:\\Users\\Dom\\Desktop\\JEDEN.txt");
+        File plik = new File("C:\\Users\\Dom\\Desktop\\pliki do JP\\JEDEN.txt");
         BufferedReader buf = new BufferedReader(new FileReader(plik));
         while ((st = buf.readLine()) != null){
             Dane = st.split("-");
-            //produkty.add(Dane[1]);
             Lista_produktow.add(new produkt(Dane[0],Dane[1],Integer.parseInt(Dane[2])));
         }
         // testy funkcji
-        System.out.println(czy_dostepny("maslo"));
+        czy_dostepny("maslo");
         dodaj_do_koszyka("chleb");
         dodaj_do_koszyka("woda");
         dodaj_do_koszyka("gruszka");
         wyrzuc_z_koszyka("woda");
-        wycen_koszyk();
+        wycenaZamowienie(koszyk);
         realizacja_zamowienia(plik);
+        dodajArtykul("012","pomarancza",69,plik);
+        pobierzArtykul("banan",plik);
         System.exit(69);
     }
 
@@ -64,7 +63,7 @@ public class Main {
         System.out.println("Nie można wyrzucić produktu z koszyka ponieważ produkt go tam nie ma");
     }
 
-    public static void wycen_koszyk() {
+    public static void wycenaZamowienie(ArrayList<String> koszyk) {
         int cena = 0;
         for (int i = 0; i < Lista_produktow.size(); i++){
             for (int j = 0; j < koszyk.size(); j++) {
@@ -101,6 +100,32 @@ public class Main {
         }
         zapisz_dane_do_pliku(plik);
         System.out.println("Zamowienie zrealizowane");
-        System.out.print("Zaktualizowano baze produktów");
+        System.out.println("Zaktualizowano baze produktów");
+    }
+
+    public static void dodajArtykul(String kod,String nazwa,int cena,File plik) throws IOException {
+        BufferedWriter writer2 = new BufferedWriter(new FileWriter(plik,true));
+        writer2.write(kod+"-"+nazwa+"-"+cena+"\n");
+        writer2.close();
+        System.out.println("Dodano produkt do sklepu: "+nazwa);
+    }
+
+    public static void pobierzArtykul(String nazwa,File plik) throws IOException {
+        Scanner in;
+        try {
+            in = new Scanner(plik);
+            while(in.hasNext())
+            {
+                String line=in.nextLine();
+                if(line.contains(nazwa)){
+                    System.out.println("Dane produktu: ");
+                    System.out.println(line);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie znaleziono pliku");
+            e.printStackTrace();
+        }
+
     }
 }
